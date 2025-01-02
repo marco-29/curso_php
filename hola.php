4ebee0f41959df9cc587d459cb3e0ffa62d44257
marco-29 <marco@jid.agency>
2025-01-01 23:45:28 -0600

    0.0.1 Variables

---
+++ hola.php
@@ -0,0 +1,5 @@
+<?php 
+  echo "Hola mundo";
+  echo "<br>";
+  print "Hola mundo";
+?>
---
+++ variable.php
@@ -0,0 +1,24 @@
+<?php
+  $nombre = "Marco";
+  $edad = 20;
+  $altura = 1.72;
+  $casado = true;
+
+  echo $nombre;
+  echo "<br>";
+  var_dump($nombre);
+  echo "<br>";
+  echo gettype($nombre);
+  echo "<br>";
+  echo $edad;
+  echo "<br>";
+  var_dump($edad);
+  echo "<br>";
+  echo $altura;
+  echo "<br>";
+  var_dump($altura);
+  echo "<br>";
+  echo $casado;
+  echo "<br>";
+  var_dump($casado);
+?>
