cff94055a0550bf20f29ec350c2381e7609d6339
marco-29 <marco@jid.agency>
2025-01-03 00:12:22 -0600

    0.0.4 OPERADORES

--- op_logicos.php
+++ op_logicos.php
@@ -1,13 +1,24 @@
 <?php
   //Variables con valores booleanos
   $x = true;
-  $y = true;
+  $y = false;
 
   //Operador and
   echo $x and $y;
   echo '<br>';
   echo $x && $y;
+  echo '<br>';
 
+  //Operador or
+  echo $x or $y;
+  echo '<br>';
+  echo $x || $y;
+  echo '<br>';
 
-
+  //Operador XOR
+  echo $x xor $y;
+  echo '<br>';
+ 
+  //Negacion
+  echo !$y;
 ?>
